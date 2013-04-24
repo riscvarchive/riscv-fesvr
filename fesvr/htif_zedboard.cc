997580cb304f13083218affa007fe296e3a54a84
Andrew Waterman <waterman@eecs.berkeley.edu>
2013-04-24 03:52:50 -0700

    use MAP_ANON instead of MAP_ANONYMOUS
    both Linux and Mac OS accept the former.

--- fesvr/htif_zedboard.cc
+++ fesvr/htif_zedboard.cc
@@ -19,7 +19,7 @@ htif_zedboard_t::htif_zedboard_t(const std::vector<std::string>& args)
   assert(dev_vaddr != MAP_FAILED);
   write_reg(31, 0); // reset
 
-  mem = (uintptr_t*)mmap(0, mem_mb() << 20, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+  mem = (uintptr_t*)mmap(0, mem_mb() << 20, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANON, -1, 0);
   assert(mem != MAP_FAILED);
 }
 
